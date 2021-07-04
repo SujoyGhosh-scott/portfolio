9b3dd7c568c1036c676b7b968dceb4f3868ad6b2
Sujoy Ghosh <ghoshsujoy336@gmail.com>
2021-07-04 17:17:09 +0530

    back again

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
@@ -15,8 +16,33 @@ export default function About() {
   return (
     <Grid container id="about" className={classes.root}>
       <Grid item sm={2} xs={false}></Grid>
-      <Grid item sm={8} xs={12}>
-        about
+      <Grid
+        container
+        justify="center"
+        direction="column"
+        alignItems="flex-start"
+        item
+        sm={8}
+        xs={12}
+      >
+        <Typography
+          variant="h4"
+          component="h3"
+          style={{ marginBottom: "1rem" }}
+        >
+          I'am <span className={"light"}>Sujoy</span>
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          component="p"
+          style={{ marginRight: "6rem" }}
+        >
+          currently pursuing my bachelors' majoring in <b>Computer Sc</b>. I'm
+          really passionate about Web Development and Looking for an opportunity
+          to work as a dedicated and enthusiastic professional where my talent
+          and knowledge can be best utilised for the development of the
+          organization.
+        </Typography>
       </Grid>
       <Grid item sm={2} xs={false}></Grid>
     </Grid>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,12 +2,30 @@ import React from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Timeline from "@material-ui/lab/Timeline";
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
+import TimelineConnector from "@material-ui/lab/TimelineConnector";
+import TimelineContent from "@material-ui/lab/TimelineContent";
+import TimelineOppositeContent from "@material-ui/lab/TimelineOppositeContent";
+import TimelineDot from "@material-ui/lab/TimelineDot";
+import Paper from "@material-ui/core/Paper";
 
-const useStyles = makeStyles({
+import FastfoodIcon from "@material-ui/icons/Fastfood";
+import LaptopMacIcon from "@material-ui/icons/LaptopMac";
+import ImportContactsIcon from "@material-ui/icons/ImportContacts";
+import ScheduleIcon from "@material-ui/icons/Schedule";
+
+const useStyles = makeStyles(() => ({
   root: {
     minHeight: "100vh",
   },
-});
+  paper: {
+    padding: "10px",
+    textAlign: "left",
+  },
+}));
 
 export default function Experience() {
   const classes = useStyles();
@@ -15,8 +33,93 @@ export default function Experience() {
   return (
     <Grid container id="experience" className={classes.root}>
       <Grid item sm={2} xs={false}></Grid>
-      <Grid item sm={8} xs={12}>
-        experience
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        item
+        sm={8}
+        xs={12}
+      >
+        <Typography variant="h3" component="p" style={{ marginBottom: "1rem" }}>
+          E<span className={"light"}>xp</span>erience
+        </Typography>
+        <Timeline align="alternate">
+          <TimelineItem>
+            <TimelineOppositeContent>
+              <Typography variant="body2" color="textSecondary">
+                April, 2020
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot color="secondary">
+                <FastfoodIcon />
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Paper elevation={3} className={classes.paper}>
+                <Typography>coding journey stated as web developer.</Typography>
+              </Paper>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent>
+              <Typography variant="body2" color="textSecondary">
+                Feb, 2021
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot color="secondary">
+                <LaptopMacIcon />
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Paper elevation={3} className={classes.paper}>
+                <Typography variant="h6" component="h1" color="secondary">
+                  Support Genie
+                </Typography>
+                <Typography>Software developer Intern</Typography>
+              </Paper>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent>
+              <Typography variant="body2" color="textSecondary">
+                Aug, 2021
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot color="secondary">
+                <ImportContactsIcon />
+              </TimelineDot>
+              <TimelineConnector className={classes.secondaryTail} />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Paper elevation={3} className={classes.paper}>
+                <Typography variant="h6" component="h1" color="secondary">
+                  Graduate
+                </Typography>
+                <Typography>B.Sc from University of Calcutta</Typography>
+              </Paper>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent>
+              <Typography variant="body2" color="textSecondary">
+                present
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot color="secondary">
+                <ScheduleIcon />
+              </TimelineDot>
+            </TimelineSeparator>
+            <TimelineContent></TimelineContent>
+          </TimelineItem>
+        </Timeline>
       </Grid>
       <Grid item sm={2} xs={false}></Grid>
     </Grid>
